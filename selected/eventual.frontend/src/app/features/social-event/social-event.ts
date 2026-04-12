5ec33864562f0b7df99a0d0e3c7d6a39d86cc33d
fred belotte <22018714+fredbelotte@users.noreply.github.com>
2026-04-12 16:42:12 -0500

    scaffold user interface for selected project

---
+++ selected/eventual.frontend/src/app/features/social-event/social-event.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-event',
+  imports: [],
+  templateUrl: './social-event.html',
+  styleUrl: './social-event.scss',
+})
+export class SocialEvent {}
