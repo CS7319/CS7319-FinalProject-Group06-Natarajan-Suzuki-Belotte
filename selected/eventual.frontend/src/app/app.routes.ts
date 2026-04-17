1fc2119673dfd27868f55fac9606cb5538caeae1
fred belotte <22018714+fredbelotte@users.noreply.github.com>
2026-04-17 15:59:26 -0500

    add event component

--- selected/eventual.frontend/src/app/app.routes.ts
+++ selected/eventual.frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { Profile } from './features/profile/profile';
 import { Search } from './features/search/search';
 import { authGuard } from './shared/auth-guard/auth-guard';
+import { SocialEvent } from './features/social-event/social-event';
 
 export const routes: Routes = [
+  { path: 'event', component: SocialEvent },
   { path: 'profile', component: Profile, canActivate: [authGuard] },
   { path: 'search', component: Search },
   { path: '', redirectTo: 'search', pathMatch: 'full' },
