8e7743444cb0eae047d9500955c9ad1f850d34e4
fred belotte <22018714+fredbelotte@users.noreply.github.com>
2026-04-17 18:29:56 -0500

    add event component

--- selected/eventual.frontend/src/app/features/social-event/social-event.ts
+++ selected/eventual.frontend/src/app/features/social-event/social-event.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SocialEventService } from './social-event.service';
+import { EventInfo } from '../../shared/data/event.data';
 
 @Component({
   selector: 'app-social-event',
@@ -6,4 +9,32 @@ import { Component } from '@angular/core';
   templateUrl: './social-event.html',
   styleUrl: './social-event.scss',
 })
-export class SocialEvent {}
+export class SocialEvent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly socialEventService = inject(SocialEventService);
+
+  socialEvent = signal<EventInfo>({} as EventInfo);
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.getSocialEvent(params['id'] || '');
+    });
+  }
+
+  getSocialEvent(query: string) {
+    this.socialEventService.get(query).subscribe({
+      error: () => {
+        this.socialEvent.set({
+          id: 2026,
+          hosts: [{ email: 'hello@smu.edu', name: 'Peruna' }],
+          name: 'Event 2026',
+          date: new Date(),
+          location: 'Dallas, TX',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ratione recusandae voluptas totam aut dicta, aspernatur velit molestiae obcaecati pariatur quas accusamus dolorum iusto blanditiis nihil, saepe a at. Nihil!',
+        });
+      },
+      next: (event) => this.socialEvent.set(event),
+    });
+  }
+}
